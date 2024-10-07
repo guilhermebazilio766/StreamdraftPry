f7ec11446d61243e6cc1404764aa7a594ce6b00a
Guilherme Gegenheimer Bazilio <guilherme.tec.aut@gmail.com>
2024-10-07 09:58:16 -0300

    Alteração da fonte de dados

--- Streamlit.py
+++ Streamlit.py
@@ -1,20 +1,31 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-# Carregar os dados da tabela Projetos
-url_projetos = "http://191.239.118.200:8080/Projetos.csv"
+
+# ID do arquivo no Google Drive (substitua pelo seu ID)
+file_id = '1-4VsPhqBBiq4gClop2pm6dDn3UqOphIz'
+
+# Gerar a URL de download do Google Drive
+url_projetos = f'https://drive.google.com/uc?export=download&id={file_id}'
+
+# Carregar os dados da tabela Projetos diretamente do Google Drive
 projetos = pd.read_csv(url_projetos, encoding='latin1', sep=";")
+
 # Contar a quantidade de projetos por cliente
 projetos_por_cliente = projetos.groupby('Cliente').size().reset_index(name='TotalProjetos')
+
 # Título da página
 st.title("Gráfico: Quantidade de Projetos por Cliente")
+
 # Criar o gráfico de barras usando Plotly
 fig = px.bar(projetos_por_cliente, x='Cliente', y='TotalProjetos',
-            title="Quantidade de Projetos por Cliente",
-            labels={'Cliente': 'Cliente', 'TotalProjetos': 'Total de Projetos'},
-            color='TotalProjetos',
-            height=600)
+             title="Quantidade de Projetos por Cliente",
+             labels={'Cliente': 'Cliente', 'TotalProjetos': 'Total de Projetos'},
+             color='TotalProjetos',
+             height=600)
+
 # Exibir o gráfico no Streamlit
 st.plotly_chart(fig)
+
 # Exibir a tabela com a contagem de projetos por cliente
 st.dataframe(projetos_por_cliente)
